42d4922e68a8dd5b178f87057533f7f3417f60a3
Simão Martins <xxsimaoxx1@gmail.com>
2025-02-19 18:06:42 +0000

    .

--- servidor/ast.py
+++
@@ -1,9 +0,0 @@
-from attr import dataclass
-from typing import Any, List, Optional, Dict
-
-@dataclass
-class Expression:
-    name: str
-    expression: str
-    value: Optional[bool]
-    lineno: int
--- servidor/lexer.py
+++ servidor/lexer.py
@@ -30,6 +30,8 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+t_ignore = ' \t'
+
 Lexer = lex.lex()
 if __name__ == '__main__':
     while True:
