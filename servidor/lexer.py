92d99b19436013f03df4e209b9653da5cdfbd3b6
Simão Martins <xxsimaoxx1@gmail.com>
2025-02-19 17:23:14 +0000

    .

--- servidor/lexer.py
+++ servidor/lexer.py
@@ -1,57 +1,44 @@
 import ply.lex as lex
 from ply.lex import TOKEN
 
-class MyLexer(object):
+tokens = (
 
-    def __init__(self):
-        self.lexer = lex.lex(module=self, debug=False)
+    'ID',
+    'ARROW',
+    'CONJ',
+    'DISJ',
+    'NOT',
+    'LPAREN',
+    'RPAREN',
+)
 
-    tokens = (
+t_ARROW = r'→'
+t_CONJ = r'∧'
+t_DISJ = r'∨'
+t_NOT = r'~'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
 
-        'ID',
-        'ARROW',
-        'CONJ',
-        'DISJ',
-        'NOT',
-        'LPAREN',
-        'RPAREN',
-    )
+t_ignore = ' \t'
 
-    t_ARROW = r'→'
-    t_CONJ = r'∧'
-    t_DISJ = r'∨'
-    t_NOT = r'~'
-    t_LPAREN = r'\('
-    t_RPAREN = r'\)'
+ident = r'[a-zA-Z][a-zA-Z0-9_]*'
 
-    t_ignore = ' \t'
+@TOKEN(ident)
+def t_ID(t):
+    return t
 
-    ident = r'[a-zA-Z][a-zA-Z0-9_]*'
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
-    @TOKEN(ident)
-    def t_ID(self, t):
-        return t
-
-    def t_newline(self, t):
-        r'\n+'
-        t.lexer.lineno += len(t.value)
-
-    def t_error(self, t):
-        print("Illegal character '%s'" % t.value[0])
-        t.lexer.skip(1)
-
-    def test(self):
-
-        while True:
-            try:
-                s = input('Enter expression: ')
-                self.lexer.input(s)
-                for token in self.lexer:
-                    print(f"Token( Type: {token.type}, Value: {token.value} )")
-
-            except EOFError:
-                break
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
 
 if __name__ == '__main__':
-    lexer = MyLexer()
-    lexer.test()
+    lexer = lex.lex()
+    while True:
+        data = input()
+        lexer.input(data)
+        for tok in lexer:
+            print(f"Token: {tok.type}, Value: {tok.value}")
