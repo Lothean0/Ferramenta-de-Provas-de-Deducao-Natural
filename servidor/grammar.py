afd50854abe0773abb9dd32df0820fd311191935
DanielAndrade53 <danielftandrade53@gmail.com>
2025-02-19 17:11:46 +0000

    First Commit

---
+++ servidor/grammar.py
@@ -0,0 +1,73 @@
+from ply import yacc
+
+from lexer import MyLexer
+
+class MyParser(object):
+
+    def __init__(self):
+        self.lexer = MyLexer()
+        self.tokens = self.lexer.tokens
+        self.parser = yacc.yacc(module=self, debug=False)
+        self.parser.exito = True
+        self.parser.ast = ''
+
+    def p_Program_1(self, p):
+        '''Program : ExpressionList'''
+        p[0] = f'{p[1]}'
+
+    def p_ExpressionsList_1(self, p):
+        '''ExpressionList : '''
+        p[0] = f''
+
+    def p_ExpressionsList_2(self, p):
+        '''ExpressionList : Expression'''
+        p[0] = f'{p[1]}'
+
+    def p_ExpressionList(self, p):
+        '''ExpressionList : Expression ExpressionList'''
+        p[0] = f'{p[1]}{p[2]}'
+
+    def p_Expression_1(self, p):
+        '''Expression : ID'''
+        p[0] = f'{p[1]}'
+
+    def p_Expression_2(self, p):
+        '''Expression : Expression BinaryOp Expression'''
+        p[0] = f'{p[1]}{p[2]}{p[3]}'
+
+    def p_BinaryOp_1(self, p):
+        '''BinaryOp : ARROW'''
+        p[0] = f'{p[1]}'
+
+    def p_BinaryOp_1(self, p):
+        '''BinaryOp : CONJ'''
+        p[0] = f'{p[1]}'
+
+    def p_BinaryOp_1(self, p):
+        '''BinaryOp : DISJ'''
+        p[0] = f'{p[1]}'
+
+    def p_BinaryOp_1(self, p):
+        '''BinaryOp : NOT'''
+        p[0] = f'{p[1]}'
+
+    def p_error(self, p):
+        if p:
+            print(f"Syntax error at token '{p.value}' (type: {p.type}) on line {p.lineno}")
+        else:
+            print("Syntax error: unexpected end of file (EOF)")
+        self.parser.exito = False
+
+if __name__ == '__main__':
+    myparser = MyParser()
+
+    while True:
+        try:
+            s = input('Enter expression: ')
+            myparser.parser.parse(s)
+
+            if myparser.parser.exito:
+                print(f'Sucess')
+
+        except EOFError:
+            break
---
+++ servidor/lexer.py
@@ -0,0 +1,57 @@
+import ply.lex as lex
+from ply.lex import TOKEN
+
+class MyLexer(object):
+
+    def __init__(self):
+        self.lexer = lex.lex(module=self, debug=False)
+
+    tokens = (
+
+        'ID',
+        'ARROW',
+        'CONJ',
+        'DISJ',
+        'NOT',
+        'LPAREN',
+        'RPAREN',
+    )
+
+    t_ARROW = r'→'
+    t_CONJ = r'∧'
+    t_DISJ = r'∨'
+    t_NOT = r'~'
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+
+    t_ignore = ' \t'
+
+    ident = r'[a-zA-Z][a-zA-Z0-9_]*'
+
+    @TOKEN(ident)
+    def t_ID(self, t):
+        return t
+
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    def t_error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    def test(self):
+
+        while True:
+            try:
+                s = input('Enter expression: ')
+                self.lexer.input(s)
+                for token in self.lexer:
+                    print(f"Token( Type: {token.type}, Value: {token.value} )")
+
+            except EOFError:
+                break
+
+if __name__ == '__main__':
+    lexer = MyLexer()
+    lexer.test()
