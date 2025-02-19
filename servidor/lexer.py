6c037ae8055bbf900aae401ab783ce4df1ad5318
Simão Martins <xxsimaoxx1@gmail.com>
2025-02-19 17:55:28 +0000

    .

--- servidor/lexer.py
+++ servidor/lexer.py
@@ -1,50 +1,34 @@
 import ply.lex as lex
 
-class MyLexer(object):
 
-    def __init__(self):
-        self.lexer = lex.lex(module=self, debug=False)
+tokens = (
+    'ID',
+    'ARROW',
+    'CONJ',
+    'DISJ',
+    'NOT',
+    'LPAREN',
+    'RPAREN',
+)
 
-    tokens = (
-        'ID',
-        'ARROW',
-        'CONJ',
-        'DISJ',
-        'NOT',
-        'LPAREN',
-        'RPAREN',
-    )
-
-    t_ARROW = r'→'
-    t_CONJ = r'∧'
-    t_DISJ = r'∨'
-    t_NOT = r'~'
-    t_LPAREN = r'\('
-    t_RPAREN = r'\)'
+t_ARROW = r'→'
+t_CONJ = r'∧'
+t_DISJ = r'∨'
+t_NOT = r'~'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
 
 def t_ID(t):
     r'[a-zA-Z][a-zA-Z0-9_]*'
     return t
 
-def t_newline(self, t):
+def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-def t_error(self, t):
+def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-
-def test(self):
-
-    while True:
-        try:
-            s = input('Enter expression: ')
-            self.lexer.input(s)
-            for token in self.lexer:
-                print(f"Token( Type: {token.type}, Value: {token.value} )")
-
-        except EOFError:
-            break
 
 Lexer = lex.lex()
 if __name__ == '__main__':
