cd2101e6e140de561eb6decd300345f1da4bfbb5
DanielAndrade53 <danielftandrade53@gmail.com>
2025-03-01 11:47:50 +0000

    Code refactorin 2

--- servidor/test/main.py
+++ servidor/test/main.py
@@ -7,34 +7,23 @@ is present in the knowledge base or wheter any of them can be deduced by
 the current contents of the knowledge base.
 """
 
-"""
-Modus Ponens (MP):
-Also know as: Implication Elimination, Affirming the Antecedent
-
-Modus Ponens is a valid rule of inference that states if you have a 
-conditional statement (p->q) and the antecedent (p) is true, then you 
-can infer that the consequent (q) is also true
-"""
-
-
 def check_knowledge_base(
         premise_expr: str,
         knowledge_base: List[str]
 ) -> bool:
     return premise_expr in knowledge_base
 
+
 def split_expression(
         logical_expr: str,
 ):
     pattern = r'([A-Za-z]+)\((.*)\)'
-
     match = re.match(pattern, logical_expr)
     if not match:
         raise ValueError(C_RED + f'[ERROR] ' + C_END + f'Invalid expression')
 
     # match.group(1) = EbinOp
     content = match.group(2) # = [ -> , EVar(p0), EBinOp(->, EBinOp(->, EVar(p0), EVar(p1)), EVar(p1)) ]"
-
     args = []
     balance = 0
     current_arg = []
@@ -62,16 +51,14 @@ def apply_axiom_rule(
         knowledge_base: List[str],
 ) -> Optional[str]:
     res = input('Seleciona Axioma: ')
-    if res in knowledge_base:
-        if res==logical_expr:
-            return 'foo'
-    return 'boo'
+    return 'foo' if res in knowledge_base and res == logical_expr else 'boo'
 
-def apply_Introduction_Implication(
+def apply_Implication_Introduction(
         logical_expr: str,
         knowledge_base: List[str]
 ) -> Optional[str]:
     arguments = split_expression(logical_expr)
+
     print(C_YELLOW + f'[DEBUG] ' + C_END + f'Arguments = {arguments}')
     if arguments:
         print(C_YELLOW + f'[DEBUG] ' + C_END + f'Antecedent = {arguments[1]}')
@@ -84,8 +71,15 @@ def apply_Introduction_Implication(
     lista_branca.append(arguments[2])
     return arguments[2]
 
+"""
+Modus Ponens (MP):
+Also know as: Implication Elimination, Affirming the Antecedent
 
-def apply_Elimination_Implication(
+Modus Ponens is a valid rule of inference that states if you have a 
+conditional statement (p->q) and the antecedent (p) is true, then you 
+can infer that the consequent (q) is also true
+"""
+def apply_Implication_Elimination(
         logical_expr: str,
         knowledge_base: List[str]
 ) -> Optional[str]:
@@ -93,7 +87,6 @@ def apply_Elimination_Implication(
         res = input('Seliona hipotese')
         if res in knowledge_base:
             lista_branca.append(res)
-
             lista_branca.append(f"EBinOp(->, {res}, {logical_expr})")
             return 'foo2'
         return ''
@@ -119,7 +112,7 @@ if __name__ == '__main__':
         name='II',
         other_names=['Introduction: Implication'],
         description='I dont know',
-        apply=apply_Introduction_Implication
+        apply=apply_Implication_Introduction,
     ))
 
 
@@ -127,7 +120,7 @@ if __name__ == '__main__':
         name="EI",
         other_names=['Elimination: Implication'],
         description='If A -> B and A is known, then infer B.',
-        apply=apply_Elimination_Implication
+        apply=apply_Implication_Elimination
     ))
 
     rule_registry.register_rule(Rule(
