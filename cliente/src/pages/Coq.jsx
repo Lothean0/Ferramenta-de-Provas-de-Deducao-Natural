0a5be6697eab24e5d46391f28899cdb122f4d60d
DanielAndrade53 <danielftandrade53@gmail.com>
2025-03-30 10:00:08 +0100

    add some files to test idea

---
+++ cliente/src/pages/Coq.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import CoqBody from "../components/CoqBody"
+
+const Coq = () => {
+  return (
+    <>
+      {/*<Header />*/}
+      <CoqBody />
+    </>
+  );
+};
+
+export default Coq;
