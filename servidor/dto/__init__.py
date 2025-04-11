89d55f0bb10811693fa33ad78c5c277ca6aafe4a
DanielAndrade53 <danielftandrade53@gmail.com>
2025-04-11 09:27:09 +0100

    Add dto test files

--- servidor/dto/__init__.py
+++
@@ -1,2 +0,0 @@
-from .user_dto import users_bp
-from .logic_dto import logic_bp
--- servidor/dto/user_dto.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-users_bp = Blueprint("users", __name__)
-
-@users_bp.route("/api/users", methods=["GET"])
-def users():
-    return jsonify({"users": ["daniel", "pedro", "simao"]})
