7e30581b1a63d1fca9248535561e9ce30260c339
DanielAndrade53 <danielftandrade53@gmail.com>
2025-04-18 19:53:57 +0100

    Refactor main.py

--- servidor/main.py
+++ servidor/main.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify
+import json
+import os
+
+from flask import Flask, jsonify, flash, redirect, url_for, send_from_directory
 from flask_cors import CORS
 from flask import request
+from werkzeug.utils import secure_filename
 
 from servidor.rules.implication.introduction import apply_implication_introduction
 from servidor.propositional_logic.propositional_logic_codegen import CodeGenerator
@@ -8,8 +12,14 @@ from servidor.propositional_logic.propositional_logic_parser import Parser
 from servidor.propositional_logic.propositional_logic_semantic import SemanticAnalyzer
 
 
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 CORS(app, origins="*")
+
 # CORS(app, resources={r"/*": {"origins": "*"}})
 
 response = []
@@ -51,6 +61,7 @@ def add_node():
 
 
         if not data.get("expression"):
+            # print("No expression provided")
             return jsonify({"error": "Missing 'expression' parameter"}), 400
 
         # Parse the expression
@@ -70,7 +81,7 @@ def add_node():
                 "name": Parser.parse(parsed_expression),
                 "parentId": "",
                 "child": [],
-                "knowledge_base": request.args.get("knowledge_base", "[]"),
+                "knowledge_base": data.get("knowledge_base", "[]"),
             })
 
         return jsonify(response), 200
@@ -79,8 +90,8 @@ def add_node():
     except Exception as e:
         return jsonify({"error": "Failed to process request", "details": str(e)}), 500
 
-@app.route("/api/result", methods=["POST"])
-def validate_expression():
+@app.route("/api/rules", methods=["POST"])
+def apply_rules():
     try:
         data = request.get_json()
         print("Received data:", data)
@@ -154,12 +165,63 @@ def validate_expression():
         return jsonify({"error": "Failed to process request", "details": str(e)}), 500
 
 
-
 @app.route("/api/reset", methods=["POST"])
 def reset_data():
+    print("Reseting data ...")
     response.clear()
     return jsonify(response), 200
 
+@app.route("/api/save", methods=["POST"])
+def save_file():
+    data = request.get_json()  # Get the data from the request
+    print("Received data:", data)
+
+    # Define the path where the file will be saved
+    file_path = 'tree_structure.txt'
+
+    # Convert the received data into a string format
+    tree_data_str = json.dumps(data, indent=2)  # Pretty-print the JSON data
+
+    # Write the string data to a .txt file
+    try:
+        with open(file_path, 'w') as file:
+            file.write(tree_data_str)
+        return jsonify({"message": "Data saved to file successfully!"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/api/file", methods=["POST"])
+def upload_file():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return jsonify({"error": "No file part"}), 400
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return jsonify({"error": "No file part"}), 400
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'r') as file:
+                treedata = file.read()
+            return jsonify({"filename": treedata}), 200
+
+    return jsonify({"error": "File not allowed"}), 400
+
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], name)
 
 @app.route("/api/teste", methods=["GET"])
 def send_data():
