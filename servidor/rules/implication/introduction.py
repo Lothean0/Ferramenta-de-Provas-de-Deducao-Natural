b25dda5b28cb7b6a0e25da302edf030c3b573955
DanielAndrade53 <danielftandrade53@gmail.com>
2025-04-21 16:52:42 +0100

    refactor intro implication

--- servidor/rules/implication/introduction.py
+++ servidor/rules/implication/introduction.py
@@ -2,17 +2,11 @@ from typing import Any
 
 from servidor.utils.my_utils import split_expression
 
-from servidor.config import n_hypothesis, knowledge_base, problems
-
 def apply_implication_introduction(
         logical_expr: str, # (p0 ->p1)
         available_hypothesis: set[str], # []
         problem: str #1
 ) -> list[dict[str, str | list[Any] | Any]]:
-
-    local_n_hypothesis = n_hypothesis
-    local_knowledge_base = knowledge_base
-    local_problems = problems
 
     arguments = split_expression(logical_expr)
 
@@ -22,25 +16,9 @@ def apply_implication_introduction(
     antecedent, consequent = arguments[1], arguments[2]
     # print(f'{antecedent} -> {consequent}')
 
-    tmp = f'X{local_n_hypothesis}'
+    local_knowledge_base = {}
+    tmp = f'X{problem}'
     local_knowledge_base[tmp] = antecedent
-    local_n_hypothesis += 1
-
-    available_hypothesis_new = available_hypothesis.copy()
-    available_hypothesis_new.add(tmp)
-
-    print(f"Before remove{local_problems}")
-    local_problems.pop(problem, None)
-    print(f"After remove{local_problems}")
-
-    print("Here 1")
-    print(consequent)
-    print(available_hypothesis_new)
-
-    local_key = str(problem) + '1'
-
-    local_problems[local_key] = (consequent, available_hypothesis_new)
-    print("Here 2")
 
     result = [
         {
