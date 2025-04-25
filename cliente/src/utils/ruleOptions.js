ff948634bc4fa58d5c2ed3abeb461424507e4b92
DanielAndrade53 <danielftandrade53@gmail.com>
2025-04-25 14:42:23 +0100

    add utils\

--- cliente/src/utils/ruleOptions.js
+++ cliente/src/utils/ruleOptions.js
@@ -1,5 +1,15 @@
 export const ruleOptions = {
     axiom: "Axioma",
+
     implication_introduction: "Intro. Implicação",
     implication_elimination: "Elim. Implicação",
+
+    conjunction_introduction: "Intro. Conjunção", 
+    conjunction_elimination_1 : "Elim. Implicação 1",
+    conjunction_elimination_2 : "Elim. Implicação 2",
+
+    disjunction_introduction_1: "Intro. Disjunção 1", 
+    disjunction_introduction_2: "Intro. Disjunção 2", 
+    
+
 };
