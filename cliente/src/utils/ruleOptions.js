b1962558f50198c63474313abb925019b11f33c6
DanielAndrade53 <danielftandrade53@gmail.com>
2025-04-25 11:58:18 +0100

    add utils\

---
+++ cliente/src/utils/ruleOptions.js
@@ -0,0 +1,5 @@
+export const ruleOptions = {
+    axiom: "Axioma",
+    implication_introduction: "Intro. Implicação",
+    implication_elimination: "Elim. Implicação",
+};
---
+++ cliente/src/utils/translations.js
@@ -0,0 +1,37 @@
+export const translations = {
+    EN: {
+      reset: "Reset",
+      addNode: "Add Node",
+      applyRule: "Apply Rule",
+      uploadOpen: "Open Upload",
+      uploadClose: "Close Upload",
+      download: "Download",
+      selectRule: "Select a rule",
+      dragDrop: "Drag and drop a file here",
+      expressionPlaceholder: "Enter expression: p0 -> (p1 -> p2)",
+      knowledgePlaceholder: "Enter Gamma values: p1, p5",
+      language: "EN",
+  
+      name: "Name",
+      parentId: "Parent ID",
+      kb: "Knowledge Base",
+    },
+    PT: {
+      reset: "Reiniciar",
+      addNode: "Adicionar Nó",
+      applyRule: "Aplicar Regra",
+      uploadOpen: "Abrir Upload",
+      uploadClose: "Fechar Upload",
+      download: "Baixar",
+      selectRule: "Selecione uma regra",
+      dragDrop: "Arraste e solte um arquivo aqui",
+      expressionPlaceholder: "Insira expressão: p0 -> (p1 -> p2)",
+      knowledgePlaceholder: "Insira valores Gamma: p0, p5",
+      language: "PT",
+  
+      name: "Nome",
+      parentId: "ID Pai",
+      kb: "Hipóteses",
+    },
+};
+  
