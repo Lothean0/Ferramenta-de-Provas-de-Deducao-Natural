4b6175160aa842903b53eb6446e9baadb1fbd82e
DanielAndrade53 <danielftandrade53@gmail.com>
2025-04-29 16:46:39 +0100

    started about page

--- cliente/src/components/AboutBody.jsx
+++ cliente/src/components/AboutBody.jsx
@@ -1,5 +1,6 @@
-import malainhopf from '../assets/img/malainho.jpg';
-import simaopf from '../assets/img/simao.jpg';
+import daniel_pfp from '../assets/img/daniel.jpg'
+import malainho_pfp from '../assets/img/malainho.jpg';
+import simao_pfp from '../assets/img/simao.jpg';
 
 import '../styles/AboutBody.css';
 
@@ -9,25 +10,67 @@ function AboutBody() {
     return (
         <div className='about-container'>
 
-            <div className='enunciado'>
-                <span style={{ color: 'black' }}>
-                    Dedução Natural designa um tipo de sistemas formais de prova, onde é permitido raciocinar no contexto de hipóteses e onde  coexistem regras para introdução e para eliminação dos conetivos da lógica em estudo. Por exemplo, este tipo de formalismo é estudado na UC de Lógica de LCC.
-                    O objetivo deste projeto é o desenvolvimento de uma ferramenta computacional para a construção assistida de provas em Dedução Natural, no contexto das lógicas clássica e intuicionista. Tal ferramenta poderá ser um instrumento útil no ensino-aprendizagem de sistemas formais de prova.
-                    No desenvolvimento da ferramenta tornar-se-á essencial modelar o conceito "estado da prova". Em particular, em cada momento, Em particular, em cada momento, o estado da prova deverá  permitir identificar os sub-problemas/sub-provas que ainda carecem de resolução.
-                    Propõe-se que a ferramenta permita ainda representar as provas construídas através de termos-lambda, com recurso à  correspondência Curry-Howard (que relaciona Dedução Natural com  linguagens de programação com tipos funcionais).  Eventuais funcionalidades adicionais da ferramenta deverão ser  discutidas no decurso do projeto.
-                    As  linguagens de programação a utilizar no desenvolvimento da ferramenta  serão escolhidas na fase inicial do projeto.
-                </span>
-
-                <div className="quadrado">
-                    <div className="cabeca">
-                        
-                        <img src={malainhopf}/>
-                        <img src={simaopf}/>
+            <div className="carousel">
+                <div className="elements">
+                    <div className="profile">
+                        <img src={daniel_pfp} alt="Daniel" />
+                        <span className="label">
+                            <strong>Daniel Andrade</strong> 
+                            <br />
+                            <span>A100057</span>
+                        </span>
                     </div>
-                    <div className="tronco">
-
+                    <div className="profile">
+                        <img src={malainho_pfp} alt="Malainho" />
+                        <span className="label">
+                            <strong>Pedro Malainho</strong>
+                            <br />
+                            <span>Not Available</span>
+                        </span>
+                    </div>
+                    <div className="profile">
+                        <img src={simao_pfp} alt="Simao" />
+                        <span className="label">
+                            <strong>Simão Ribeiro</strong>
+                            <br />
+                            <span>Not Available</span>
+                        </span>
                     </div>
                 </div>
+            </div>
+
+
+            <div className='statement'>
+                <h2 className="statement-title">Enunciado</h2>
+                <span style={{ color: 'black' }}>
+                    <p style={{ textIndent: '20px' }}>
+                        Dedução Natural designa um tipo de sistemas formais de prova, onde é permitido raciocinar no contexto de hipóteses e onde  coexistem regras para introdução e para eliminação dos conetivos da lógica em estudo. Por exemplo, este tipo de formalismo é estudado na UC de Lógica de LCC.
+                    </p>
+                    <p style={{ textIndent: '20px' }}>
+                        O objetivo deste projeto é o desenvolvimento de uma ferramenta computacional para a construção assistida de provas em Dedução Natural, no contexto das lógicas clássica e intuicionista. Tal ferramenta poderá ser um instrumento útil no ensino-aprendizagem de sistemas formais de prova.
+                    </p>
+                    <p style={{ textIndent: '20px' }}>
+                        No desenvolvimento da ferramenta tornar-se-á essencial modelar o conceito "estado da prova". Em particular, em cada momento, Em particular, em cada momento, o estado da prova deverá  permitir identificar os sub-problemas/sub-provas que ainda carecem de resolução.
+                    </p>
+                    <p style={{ textIndent: '20px' }}>
+                        Propõe-se que a ferramenta permita ainda representar as provas construídas através de termos-lambda, com recurso à  correspondência Curry-Howard (que relaciona Dedução Natural com  linguagens de programação com tipos funcionais).  Eventuais funcionalidades adicionais da ferramenta deverão ser  discutidas no decurso do projeto.
+                        As  linguagens de programação a utilizar no desenvolvimento da ferramenta  serão escolhidas na fase inicial do projeto.
+                    </p>
+                    <p style={{ textIndent: '20px' }}>
+                        As  linguagens de programação a utilizar no desenvolvimento da ferramenta  serão escolhidas na fase inicial do projeto.
+                    </p>
+                </span>
+            </div>
+
+            <div className='report'>
+                <h2 className="report-title">Report</h2>
+                <a href='../assets/docs/............pdf' download="report.pdf" style={{ color: 'blue', textDecoration: 'underline' }}>
+                        Download the report (need to add to the path ../assets/docs/ .pdf)
+                    </a>
+            </div>
+
+            <div className='misc-information'>
+                <p>© 2025 Ferramenta de Dedução Natural — Todos os direitos reservados.</p>
             </div>
 
         </div>
