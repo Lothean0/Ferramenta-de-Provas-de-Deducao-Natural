38588af91f0ecbeba5e0662bf710aeb907c17310
DanielAndrade53 <danielftandrade53@gmail.com>
2025-05-15 16:57:52 +0100

    minor changes

--- cliente/src/components/PropositionalLogicBody.jsx
+++ cliente/src/components/PropositionalLogicBody.jsx
@@ -3,15 +3,11 @@ import axios from 'axios';
 import '../styles/PropositionLogicBody.css';
 import { FiCheck } from "react-icons/fi";
 
-import landscape1 from '../assets/img/landscape-1.png';
-import ImageWithTooltip from './ImageWithTooltip';
-
-
+import Info from './Info';
 import NodeTreeRender from './NodeTreeRender';
 import Warning from './Warning';
 import ActionButton from './ActionButton';
 import UploadArea from './UploadArea';
-import SelectedNodeDetails from './SelectedNodeDetails';
 
 import { translations } from '../utils/translations';
 import { ruleOptions } from '../utils/ruleOptions';
@@ -536,102 +532,7 @@ function PropositionLogicBody() {
                 ) : (
                     <>
 
-                        <div className='information-container'>
-
-                            <h1 className='main-tittle'>{t("propositional_logic_tittle")} </h1>
-                            
-                            <p className='information'>{t("about_propositional_logic")}</p>
-                        
-                            <h2 className='sub-tittle'>{t("input_syntax")}</h2>
-
-                            <p className='information'>{t("about_input_syntax")}</p>
-
-                            <h2 className='sub-tittle'>{t("suported_rules")}</h2>
-
-                            
-                            <div className='rules__container'
-                                style={{ 
-                                    display: 'grid',
-                                    gridTemplateColumns: 'repeat(6, 1fr)',
-                                    gap: '2rem',
-                                    rowGap: '3.5rem',
-                                    marginLeft: '50px',
-                                    marginRight: '70px',
-                                    marginTop: '550px',
-
-                                    fontFamily: "'GochiHand', sans-serif",
-                                    position: 'absolute',
-                                    background: 'transparent'
-                                }}
-                            >
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("axiom")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("implication_introduction")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("implication_elimination")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("conjunction_introduction")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("conjunction_elimination_1")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("conjunction_elimination_2")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("disjunction_introduction_1")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("disjunction_introduction_2")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("disjunction_elimination")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("negation_introduction")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("negation_elimination")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("equivalence_introduction")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("equivalence_elimination_1")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("equivalence_elimination_2")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("RAA")}
-                                />
-                                <ImageWithTooltip
-                                    src={landscape1} 
-                                    alt={t("absurd_elimination")}
-                                />
-                                
-                            </div>
-                            
-                        </div>
+                        <Info />
                     </>
                 )}
             </div>
