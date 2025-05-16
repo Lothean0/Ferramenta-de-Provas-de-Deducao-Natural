9c243edb330cd75fca7f98fc042eed0cbddc3e03
DanielAndrade53 <danielftandrade53@gmail.com>
2025-05-16 15:10:49 +0100

    nothing

--- servidor/rules/conjunction/elimination.py
+++ servidor/rules/conjunction/elimination.py
@@ -4,8 +4,46 @@ from servidor.propositional_logic.propositional_logic_codegen import CodeGenerat
 from servidor.propositional_logic.propositional_logic_parser import Parser
 from servidor.propositional_logic.propositional_logic_semantic import SemanticAnalyzer
 
-
 def apply_conjunction_elimination_1(
+        logical_expr: str,
+        available_hypothesis: set[tuple[str, Any]],  # Corrected type hint
+        problem_id: str,
+        auxiliar_formula: str
+) -> list[dict[str, str | list[Any] | Any]]:
+    available_hypothesis_dict = dict(available_hypothesis)
+
+    try:
+        new_problem = available_hypothesis_dict.get(auxiliar_formula.upper(), auxiliar_formula)
+
+        new_problem_parsed = CodeGenerator().generate_code(
+            SemanticAnalyzer().analyze(
+                Parser.parse(new_problem, debug=False)
+            )
+        )
+
+        if not new_problem_parsed:
+            raise ValueError("Auxiliary formula cannot be empty.")
+
+        result = [
+            {
+                "name": f"EBinOp(∧, {logical_expr}, {new_problem_parsed})",
+                "parentId": "",
+                "child": [],
+                "knowledge_base": [],
+                "rule": "CE_1",
+            }
+        ]
+
+        return result
+
+
+    except Exception as e:
+        print(f"Exception in disjunction elimination: {e}")
+        raise
+
+
+
+def apply_conjunction_elimination_2(
         logical_expr: str,
         available_hypothesis: set[tuple[str, Any]],  # Corrected type hint
         problem_id: str,
@@ -32,46 +70,8 @@ def apply_conjunction_elimination_1(
                 "parentId": "",
                 "child": [],
                 "knowledge_base": [],
-                "rule": "CE_1",
-            },
-        ]
-
-        return result
-
-
-    except Exception as e:
-        print(f"Exception in disjunction elimination: {e}")
-        raise
-
-
-def apply_conjunction_elimination_2(
-        logical_expr: str,
-        available_hypothesis: set[tuple[str, Any]],  # Corrected type hint
-        problem_id: str,
-        auxiliar_formula: str
-) -> list[dict[str, str | list[Any] | Any]]:
-    available_hypothesis_dict = dict(available_hypothesis)
-
-    try:
-        new_problem = available_hypothesis_dict.get(auxiliar_formula.upper(), auxiliar_formula)
-
-        new_problem_parsed = CodeGenerator().generate_code(
-            SemanticAnalyzer().analyze(
-                Parser.parse(new_problem, debug=False)
-            )
-        )
-
-        if not new_problem_parsed:
-            raise ValueError("Auxiliary formula cannot be empty.")
-
-        result = [
-            {
-                "name": f"EBinOp(∧, {logical_expr}, {new_problem_parsed})",
-                "parentId": "",
-                "child": [],
-                "knowledge_base": [],
                 "rule": "CE_2",
-            }
+            },
         ]
 
         return result
