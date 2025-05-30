c000feda4f7e5e1a2fe564546f064d9e9962f854
DanielAndrade53 <danielftandrade53@gmail.com>
2025-05-30 14:23:25 +0100

    small changes

--- cliente/src/pages/HomePage.jsx
+++ cliente/src/pages/HomePage.jsx
@@ -1,15 +1,26 @@
+import { useMemo, useState } from "react";
+
 import Header from "../components/Header";
 import Home from "../components/Home";
 
-function HomePage() {
+import { translations } from "../utils/translations";
+
+const HomePage = () => {
+  const [language, setLanguage] = useState('PT');
   
-    return (
-        <>
-            <Header/>
-            <Home/>
-        </>
-    )
-  }
+  const t = useMemo(() => (key) => translations[language][key], [language]);
+
+  const handleLanguageToggle = (selectedLanguage) => {
+    setLanguage(selectedLanguage);
+  };
+
+  return (
+    <>
+        <Header translate={t} />
+        <Home/>
+    </>
+    );
+};
   
 export default HomePage;
   
